9b0b491e0155e06a9ecadb27f98f8db6ff0f3541
wolfrage76 <75458290+wolfrage76@users.noreply.github.com>
2025-01-19 07:35:29 -0500

    Added Slack integration

--- utilities/notifications.py
+++ utilities/notifications.py
@@ -15,6 +15,7 @@ class NotificationService:
                 - telegram_chat_id (str): Telegram chat ID.
                 - pushover_user_key (str): Pushover user key.
                 - pushover_app_token (str): Pushover app token.
+                - slack_webhook (str): Slack webhook URL.
         """
         self.discord_webhook = config.get('discord_webhook')
         self.pushbullet_token = config.get('pushbullet_token')
@@ -23,6 +24,7 @@ class NotificationService:
         self.pushover_user_key = config.get('pushover_user_key')
         self.pushover_app_token = config.get('pushover_app_token')
         self.webhook_url = config.get('webhook_url')
+        self.slack_webhook = config.get('slack_webhook')
 
     def notify(self, message, shared_state=None):
         """
@@ -33,35 +35,36 @@ class NotificationService:
             self.send_discord_notification(message)
             
         if self.pushbullet_token:
-            self.send_pushbullet_notification(message.replace(separator,'').replace(separator,'')  )
+            self.send_pushbullet_notification(message.replace(separator, ''))
             
         if self.telegram_bot_token and self.telegram_chat_id:
-            self.send_telegram_notification(message.replace(separator,'').replace(separator,'')  )
+            self.send_telegram_notification(message.replace(separator, ''))
             
         if self.pushover_user_key and self.pushover_app_token:
-            self.send_pushover_notification(message.replace(separator,'').replace(separator,'')  )
+            self.send_pushover_notification(message.replace(separator, ''))
+        
         if self.webhook_url:
-            self.send_shared_state_webhook(shared_state )
+            self.send_shared_state_webhook(shared_state)
+        
+        if self.slack_webhook:
+            self.send_slack_notification(message.replace(separator, ''))
 
-
-    def send_shared_state_webhook(self,shared_state):
+    def send_shared_state_webhook(self, shared_state):
         """
         Sends the shared_state object as a JSON payload to the specified webhook URL.
 
         Args:
             shared_state (dict): The shared state object to send.
-            webhook_url (str): The destination webhook URL.
 
         Returns:
             bool: True if the webhook was sent successfully, False otherwise.
         """
-        webhook_url = self.webhook_url
         try:
             headers = {'Content-Type': 'application/json'}
             payload = json.dumps(shared_state, indent=2)
             
-            logging.debug(f"Sending shared state to webhook URL: {webhook_url}")
-            response = requests.post(webhook_url, headers=headers, data=payload)
+            logging.debug(f"Sending shared state to webhook URL: {self.webhook_url}")
+            response = requests.post(self.webhook_url, headers=headers, data=payload)
             
             if response.status_code == 200:
                 logging.debug("Webhook sent successfully.")
@@ -73,7 +76,6 @@ class NotificationService:
             logging.error(f"Error sending webhook: {e}")
             return False
 
-        
     def send_discord_notification(self, message):
         """
         Send a notification to Discord using a webhook.
@@ -131,5 +133,15 @@ class NotificationService:
             logging.debug("Pushover notification sent successfully.")
         except Exception as e:
             logging.error(f"Error sending Pushover notification: {e}")
-    
-    
+
+    def send_slack_notification(self, message):
+        """
+        Send a notification to Slack using a webhook.
+        """
+        try:
+            payload = {"text": message}
+            response = requests.post(self.slack_webhook, json=payload)
+            response.raise_for_status()
+            logging.debug("Slack notification sent successfully.")
+        except Exception as e:
+            logging.error(f"Error sending Slack notification: {e}")
