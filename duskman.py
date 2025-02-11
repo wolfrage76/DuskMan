a793285bc48ad1c06b7bd94f5511f7ca5608f58b
wolfrage76 <75458290+wolfrage76@users.noreply.github.com>
2025-02-11 12:42:51 -0500

    Add additional market info

--- duskman.py
+++ duskman.py
@@ -75,10 +75,7 @@ args = parser.parse_args() or {}
 # Store it as a boolean variable
 display_gui = not args.d
 
-if config.get('use_sudo', False):
-    use_sudo = 'sudo'
-else:
-    use_sudo = ''
+use_sudo = 'sudo' if config.get('use_sudo', False) else ''
 
 errored = False
 log_entries = []
@@ -183,6 +180,16 @@ def get_env_variable(var_name='WALLET_PASSWORD', dotenv_key='WALLET_PASSWORD'):
     return value
 
 password = get_env_variable(config.get('pwd_var_name', 'WALLET_PASSWORD'), dotenv_key="WALLET_PASSWORD")
+
+def format_number(number: int) -> str:
+    return f"{number:,}"
+
+def convert_timestamp(timestamp: str) -> str:
+    try:
+        dt = datetime.fromisoformat(timestamp.replace('Z', '+00:00'))
+        return dt.strftime('%m/%d/%y')
+    except ValueError as e:
+        return f"Error: Invalid timestamp format - {e}"
 
 
 def display_wallet_distribution_bar(public_amount, shielded_amount, width=30):
@@ -339,7 +346,7 @@ async def fetch_dusk_data():
 
 
 def format_float(value, places=4):
-    """Convert float to a string with max 4 (default) decimal digits."""
+    """Convert float to a string with max (4 default) decimal digits."""
     parts = str(convert_to_float(value)).split('.')
     if len(parts) == 2:
         return f"{parts[0]}.{parts[1][:places]}" if len(parts[1]) > 0 else parts[0]
@@ -361,9 +368,7 @@ def log_action(action="Action", details="No Details", type='info'):
     Write log messages to specific files based on type.
     """
     
-    # Create a timestamp
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M")
-    # Format the message
     formatted_message = LOG_FORMAT.format(timestamp=timestamp, message=f"{action}: {details}")
     
     formatted_message = formatted_message.replace(password, '#####').replace(password, '#####')
@@ -684,7 +689,7 @@ async def frequent_update_loop():
                     shared_state["stake_info"]["reclaimable_slashed_stake"] = r_slashed or 0.0
                     shared_state["stake_info"]["rewards_amount"] = a_rewards or 0.0
                 
-                await fetch_dusk_data()  
+                await fetch_dusk_data()
                     
                 loopcnt = 0  # Reset loop count after update
             
@@ -1012,9 +1017,15 @@ async def realtime_display(tmux=False):
                 ath_change = shared_state["ath_change_percentage"]
                 ath_date = shared_state["ath_date"]
                 
+                if shared_state['market_cap_change_percentage_24h'] > 0:
+                    mcap_color = GREEN
+                else:
+                    mcap_color = RED
+                
                 atl = shared_state["atl"]
                 atl_date = shared_state["atl_date"]
-                
+                mcap = f'{LIGHT_WHITE}24hr Volume: ${format_number(volume)}  Market Cap: ${format_number(mkt_cap)} ({mcap_color}{mkt_cap_change:.2f}%{LIGHT_WHITE})\n'
+                athl = f' {LIGHT_WHITE}ATH: ${format_float(ath)} ({ath_change:.2f}%) {convert_timestamp(ath_date)} | ATL: ${format_float(atl)} {convert_timestamp(atl_date)}\n'
                 ######################
                 
                 top_bar = f" {LIGHT_WHITE}======={DEFAULT} {currenttime} Block: {LIGHT_BLUE}#{blk} {DEFAULT}(E: {LIGHT_BLUE}{epoch_num}{DEFAULT}) Peers: {peercolor}{shared_state['peer_count']}{DEFAULT} {LIGHT_WHITE}=======\n"
@@ -1054,6 +1065,9 @@ async def realtime_display(tmux=False):
                     f"    {YELLOW}Rewards{DEFAULT}       | {YELLOW}{format_float(st_info['rewards_amount'])} ({LIGHT_BLUE}{reward_percent:.4f}%{DEFAULT}) (${format_float(st_info['rewards_amount'] * price, 2)}) {LIGHT_WHITE}{per_epoch}{DEFAULT}\n"
                     f"    {LIGHT_RED}Reclaimable{DEFAULT}   | {LIGHT_RED}{format_float(st_info['reclaimable_slashed_stake'])} (${format_float(st_info['reclaimable_slashed_stake'] * price, 2)}){DEFAULT}\n"
                     f" {LIGHT_WHITE}{('=' * (len(remove_ansi(top_bar)) - 2))}{DEFAULT}\n"  
+                    f"  {mcap} {athl}\n"
+                    
+                    
                 )
                 
                 if include_rendered:
